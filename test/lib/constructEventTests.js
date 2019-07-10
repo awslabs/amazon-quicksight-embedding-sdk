0cf075ecc054f19a71e341fbb72cd19e239daee9
Michael "Gilli" Gilliland <michael.gilliland@arcadia.io>
2019-07-10 12:14:26 -0400

    Fix compatability with IE 11.
    `Object.values` and `new Set` calls with constructor arguments are not
    supported in IE 11.
    
    I fixed this by writing constructEvent in an IE 11 compatable way. There
    may be some "babel way" of resolving this but I'm not really sure how
    that would look.
    
    I added the specs to ensure my change didn't introduce regressions (the
    tests passed before and after my change).

---
+++ test/lib/constructEventTests.js
@@ -0,0 +1,27 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import constructEvent from '../../src/lib/constructEvent';
+import {OUT_GOING_POST_MESSAGE_EVENT_NAMES} from '../../src/lib/constants';
+
+import {assert} from 'chai';
+
+describe('constructEvent', () => {
+    it('works given a known event type', () => {
+        const knownEventName = Object.values(OUT_GOING_POST_MESSAGE_EVENT_NAMES)[0];
+        const payload = {hi: 'there'};
+
+        assert.deepEqual(
+            constructEvent(knownEventName, payload),
+            {
+                eventName: knownEventName,
+                clientType: 'EMBEDDING',
+                payload
+            }
+        );
+    });
+
+    it('errors given an unknown event type', () =>
+        assert.throws(() => constructEvent('bogusEvent', {}), 'Unexpected eventName')
+    );
+});
